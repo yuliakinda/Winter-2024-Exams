47accaec94b68f8bd060bd4e55623e8dda6afb31
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 13:53:37 +0200

    'use strict', const, remove unneeded code, rename variables

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,19 +1,20 @@
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
-      return;
-    } else {
-      return;
-      delete incomingvaluesarray[Z];
+// step 1
+// 'use strict', const, remove unneeded code, rename variables
+
+'use strict';
+
+const except = (incomingValuesArray, ...keysToRemove) => {
+  const keysToKeep = Object.keys(incomingValuesArray);
+
+  keysToKeep.forEach((key) => {
+    if (keysToRemove.includes(key)) {
+      delete incomingValuesArray[key];
     }
-  }, 2000);
-  ({ key: 'value' });
-  return incomingvaluesarray;
+  });
+
+  return incomingValuesArray;
 };
 
-module.exports = EXCEPT;
+module.exports = except;

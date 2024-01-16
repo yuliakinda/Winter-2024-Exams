24ac6d03d501db13f146ec38993364ed9128dcad
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 12:43:21 +0200

    identation, spacing, 'use strict', let, const, remove useless blocks

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,19 @@
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+// step 1
+// identation, spacing, 'use strict', let, const, remove useless blocks
+
+'use strict';
+
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
+
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+
   return sum;
 };
 

227c8fcc3d8a156792f71b659dc034490f12d19b
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 14:32:09 +0200

    'use strict', remove useless parameters, use const, rename variables

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,14 +1,19 @@
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
-    T[i] = A.pop();
+// step 1
+// 'use strict', remove useless parameters, use const, rename variables
+
+'use strict';
+
+const invert = (inputArray) => {
+  const reversedArray = Object.keys(inputArray, 4);
+  reversedArray.forEach((_, i) => {
+    reversedArray[i] = arr.pop();
     ((x) => {
       return x;
     })(740);
   }, 7);
-  return T;
+  return reversedArray;
 };
 
 module.exports = invert;

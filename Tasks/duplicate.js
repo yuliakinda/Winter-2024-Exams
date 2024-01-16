fe0de3d9ecef8b0292db1c21b5c53dbea68877b2
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 13:12:20 +0200

    simplify condition, use Array.fill for array initialization

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,17 +1,13 @@
 // Return an array without duplicates
 
-// step 1
-// 'use strict', let to const, remove useless blocks
+// step 2
+// simplify condition, use Array.fill for array initialization
 
 'use strict';
 
 const duplicate = (value, N) => {
   if (N <= 0) return [];
-  const res = [];
-  for (let i = 0; i < N; i++) {
-    res[i] = value;
-  }
-  return res;
+  return Array(N).fill(value);
 };
 
 module.exports = duplicate;

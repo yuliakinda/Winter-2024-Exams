a7e2d4b47f245372e3e72f442be98f0f76e32adc
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 12:01:19 +0200

    identation, spacing, 'use strict', renaming variables

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,26 @@
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
+// step 1
+// identation, spacing, 'use strict', renaming variables
+
+'use strict';
+
+let getValueBetween = (inputString, prefix, suffix) => {
+  let i = inputString.indexOf(prefix);
   if (i === -1) return '';
   else {
-    k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
+    let k = i + prefix.length;
+    inputString = inputString.substring(k);
+    if (suffix) {
+      i = inputString.indexOf(suffix);
       if (i === -1) {
         return '';
       } else {
-        str = str.substring(0, i);
+        inputString = inputString.substring(0, i);
       }
     }
   }
-  return str;
+  return inputString;
 };
 
-module.exports = getvaluebetween;
+module.exports = getValueBetween;

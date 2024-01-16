bade01ec62e457f1f6fb7dfb99bd4f6b3aee4685
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 14:11:44 +0200

    simplify checking if the value is a number

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,13 +1,13 @@
 // Increment all numbers in dictionary
 
-// step 1
-// 'use strict', const, remove rest_variables, rename other variables
+// step 2
+// simplify checking if the value is a number
 
 'use strict';
 
 const incrementNumbers = (dictionary) => {
   for (const key in dictionary) {
-    if ((typeof dictionary[key]).charAt(0).toUpperCase() === 'N') {
+    if (typeof dictionary[key] === 'number') {
       dictionary[key] = dictionary[key] + 1;
     }
   }

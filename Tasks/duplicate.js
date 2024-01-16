624a17368bb9d718a6f4b0b7fdd28f58ebecbb57
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 13:10:30 +0200

    'use strict', let to const, remove useless blocks

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,17 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
+// step 1
+// 'use strict', let to const, remove useless blocks
+
+'use strict';
+
+const duplicate = (value, N) => {
   if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+  const res = [];
+  for (let i = 0; i < N; i++) {
+    res[i] = value;
   }
+  return res;
 };
 
 module.exports = duplicate;

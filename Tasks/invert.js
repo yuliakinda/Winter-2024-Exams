ab96debd3b50f3be293055c90a4bd77b4043c914
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-16 14:37:58 +0200

    declare an empty array, change forEach to for...of, remove useless blocks

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,18 +1,17 @@
 // Reverse an array, you can't use .reverse()
 
-// step 1
-// 'use strict', remove useless parameters, use const, rename variables
+// step 2
+// declare an empty array, change forEach to for...of, remove useless blocks
 
 'use strict';
 
 const invert = (inputArray) => {
-  const reversedArray = Object.keys(inputArray, 4);
-  reversedArray.forEach((_, i) => {
-    reversedArray[i] = arr.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
+  const reversedArray = [];
+
+  for (const element of inputArray) {
+    reversedArray.unshift(element);
+  }
+
   return reversedArray;
 };
 

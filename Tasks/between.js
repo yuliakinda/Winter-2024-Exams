aab47a4c8cd69403f300d20a343fab0feac38a3d
Yulianna Vakhnina <yuliannabd@gmail.com>
2024-01-17 14:25:42 +0200

    no changing incoming args, removed dittos, useless variables and if

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,26 +1,19 @@
 // Extract substring between prefix and suffix
 
-// step 3
-// const where possible (forgot to add to step 2), add variable substring
+// step 4
+// no changing incoming args, removed dittos, useless variables and if
 
 'use strict';
 
 const getValueBetween = (inputString, prefix, suffix) => {
-  const i = inputString.indexOf(prefix);
-  if (i === -1) return '';
-  
-  let k = i + prefix.length;
-  inputString = inputString.substring(k);
-  
-  if (suffix) {
-    const i = inputString.indexOf(suffix);
-    if (i === -1) return '';
-    inputString = inputString.substring(0, i);
-  }
+  const prefixIndex = inputString.indexOf(prefix);
+  const startIndex = prefixIndex + prefix.length;
 
-  const substring = inputString;
+  const suffixIndex = inputString.indexOf(suffix);
 
-  return substring;
+  if (prefixIndex === -1 || suffixIndex === -1) return '';
+
+  return inputString.substring(startIndex, suffixIndex);
 };
 
 module.exports = getValueBetween;
